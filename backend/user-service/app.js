36301b2fbe827eadcc00df1d63024d5878d9c4f7
said <saiid.kachoud@gmail.com>
2025-03-12 16:45:53 +0100

    fix cors and env

--- backend/user-service/app.js
+++ backend/user-service/app.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const dbConnect = require('./config/db');
-const cors = require("cors")
-require('dotenv').config();
+const express = require("express");
+const dbConnect = require("./config/db");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 
-dbConnect()
-app.use(cors({
-    'origin' : process.env.FRONTEND_URL
-}))
+dbConnect();
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+  })
+);
 
-app.use(express.json())
-app.use('/auth',require('./routes/auth'));
+app.use(express.json());
+app.use("/auth", require("./routes/auth"));
 
-app.listen(process.env.PORT, () =>{
-    console.log(`User service is running on port ${process.env.PORT}`);
-})
+app.listen(process.env.PORT, () => {
+  console.log(`User service is running on port ${process.env.PORT}`);
+});
