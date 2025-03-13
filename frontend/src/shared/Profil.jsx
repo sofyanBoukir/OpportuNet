b92fd3814dda87eef855468440a911c7956443d6
said <saiid.kachoud@gmail.com>
2025-03-13 15:27:44 +0100

    fix

---
+++ frontend/src/shared/Profil.jsx
@@ -0,0 +1,33 @@
+export const Profil = () => {
+  return (
+    <>
+      <div className="w-full fixed bg-[#F4F2EE] h-screen"></div>
+
+      <div className="w-[45%] bg-amber-500 h-[600px] rounded-md absolute top-[70px] left-[325px] z-10"></div>
+      <div className="w-[50%] bg-amber-500 h-[600px] rounded-md absolute top-[700px] left-[100px] z-10">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Error molestiae
+        similique tempora ut itaque et molestias mollitia in, explicabo dolores
+        cupiditate magni perspiciatis, distinctio fuga magnam repellendus
+        ducimus doloribus aut laborum eum nostrum eaque iure! Esse quisquam fuga
+        animi voluptas quis! A, alias officiis? Obcaecati suscipit, nam
+        accusamus veritatis sunt eligendi ducimus laboriosam natus harum! Quos,
+        totam consequatur itaque, debitis quas veniam dolorum perspiciatis
+        accusamus ut ratione rerum architecto praesentium voluptatem voluptate
+        nulla modi incidunt cum quod id hic expedita autem ipsam repudiandae?
+        Voluptas consequatur delectus incidunt eos facere aliquid accusamus et
+        earum ipsa, deserunt non doloribus nostrum libero similique officiis,
+        quod neque dolore fugiat repellendus? Doloribus blanditiis sequi
+        distinctio animi natus obcaecati ullam tenetur ea deleniti at facere
+        officia, cum culpa similique, omnis neque velit dolores repellendus!
+        Error repellat dolores temporibus, voluptates atque modi consequatur
+        architecto, saepe et assumenda excepturi officia. Cupiditate quis
+        doloribus dolore enim sunt ipsam distinctio, modi natus exercitationem
+        eius nemo magnam alias. Adipisci maiores mollitia quidem distinctio
+        voluptatum voluptatibus repellat blanditiis quasi, nihil minima a magni
+        harum vel dolorum itaque cumque animi voluptate, rem non sed officiis
+        molestias? Fugiat expedita rerum tenetur fuga eaque assumenda,
+        voluptatum beatae quas alias, odit labore a voluptate non doloribus!
+      </div>
+    </>
+  );
+};
