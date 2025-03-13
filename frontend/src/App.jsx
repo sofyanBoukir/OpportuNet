125b824b32df6540db29c77e88f039716175dd47
mhainid <ayoubmhainid01@gmail.com>
2025-03-13 22:17:09 +0000

    post && modalPost

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Route, Routes } from "react-router-dom"
-import { Signup } from "./pages/auth/Signup"
-import { NewPassword } from "./pages/auth/NewPassword"
-import { VerifyCode } from "./pages/auth/VerifyCode"
-import { ForgotPassword } from "./pages/auth/ForgotPassword"
-import { Login } from "./pages/auth/Login"
-=======
 import { Route, Routes } from "react-router-dom";
 import { Signup } from "./pages/auth/Signup";
 import { NewPassword } from "./pages/auth/NewPassword";
@@ -14,19 +6,15 @@ import { ForgotPassword } from "./pages/auth/ForgotPassword";
 import { Login } from "./pages/auth/login";
 import { Layout } from "./layouts/Layout";
 import { Home } from "./pages/user/Home";
+import { Post } from "./components/App/Post";
+import { PostModal } from "./components/modals/PostModal";
 
->>>>>>> c315512d29e4584a9c8945993c408e84ae896997
 function App() {
   return (
     <>
       <Routes>
-<<<<<<< HEAD
-        <Route path="/user/sign_up" element={<Signup />}/>
-        <Route path="/user/sign_in" element={<Login />}/>
-=======
         <Route path="/user/sign_in" element={<Signup />} />
         <Route path="/user/login" element={<Login />} />
->>>>>>> c315512d29e4584a9c8945993c408e84ae896997
         <Route path="/user/new_password/:token" element={<NewPassword />} />
         <Route path="/user/forgot_password" element={<ForgotPassword />} />
 
--- frontend/src/components/UI/Input.jsx
+++ frontend/src/components/UI/Input.jsx
@@ -10,16 +10,6 @@ export const Input = ({
 }) => {
   return (
     <input
-<<<<<<< HEAD
-        type={type}
-        name={name}
-        placeholder={placeholder}
-        value={value}
-        onChange={onChange}
-        required={required}
-        maxLength={maxLength}
-        className={`text-black font-semibold text-md bg-inherit ${className}`}
-=======
       type={type}
       name={name}
       placeholder={placeholder}
@@ -28,7 +18,6 @@ export const Input = ({
       required={required}
       maxLength={maxLength}
       className={`text-black text-md ${className}`}
->>>>>>> c315512d29e4584a9c8945993c408e84ae896997
     />
   );
 };
