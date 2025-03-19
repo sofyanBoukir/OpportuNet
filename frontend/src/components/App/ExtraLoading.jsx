39aa2c8e9e9f3c3f192403275619e7da3a4ed787
sofyan <soufianeboukir0@gmail.com>
2025-03-19 18:49:38 +0000

    isNotified || isMessages css

--- frontend/src/components/App/ExtraLoading.jsx
+++ frontend/src/components/App/ExtraLoading.jsx
@@ -3,7 +3,7 @@ import appLogo from '../../../public/appLogo.png'
 import { LinearProgress } from '@mui/material'
 export const ExtraLoading = () => {
   return (
-    <div className='fixed inset-0 flex items-center justify-center z-50 bg-white'>
+    <div className='fixed inset-0 flex pt-[15%] justify-center z-50 bg-white'>
         <div className="w-[15%] text-center">
             <img src={appLogo} className="mx-auto" />
             <LinearProgress />
