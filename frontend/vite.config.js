99d28ef773c597ab13fd9f499f4b1e8a44ac488b
sofyan <soufianeboukir0@gmail.com>
2025-03-22 11:55:57 +0000

    accedate frontend from ip adres

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
+  // server: {
+  //   host: '0.0.0.0'
+  // },
   content: [
     "./src/app/**/*.{js,jsx,ts,tsx}",
     "./src/pages/**/*.{js,jsx,ts,tsx}",
