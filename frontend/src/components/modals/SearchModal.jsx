713ab0ec66306895375b48a86b1ed5057d973df1
said <saiid.kachoud@gmail.com>
2025-03-26 18:58:41 +0100

    searchModal complet

--- frontend/src/components/modals/SearchModal.jsx
+++ frontend/src/components/modals/SearchModal.jsx
@@ -17,6 +17,7 @@ export const SearchModal = ({ query, openSearchModal }) => {
   const [error, setError] = useState(null);
   const [suggesstedUsers, setSuggesstedUsers] = useState([]);
   const [notification, setNotification] = useState(null);
+  const [isVide, setIsVide] = useState(false);
 
   const navigate = useNavigate();
 
@@ -46,11 +47,13 @@ export const SearchModal = ({ query, openSearchModal }) => {
   useEffect(() => {
     const _getserachUsers = async () => {
       setError(null);
+      setIsVide(false);
       try {
         const response = await getserachUsers(
           localStorage.getItem("token"),
           query
         );
+
         setUsersSearch(response.data.users);
       } catch (err) {
         err.response
@@ -61,7 +64,7 @@ export const SearchModal = ({ query, openSearchModal }) => {
             });
       }
     };
-    query !== "" && _getserachUsers();
+    query !== "" ? _getserachUsers() : setIsVide(true);
   }, [query]);
 
   const handleClickUser = (user) => {
@@ -73,39 +76,47 @@ export const SearchModal = ({ query, openSearchModal }) => {
     <div className="w-full">
       <div className="fixed w-full inset-0 bg-black/50 backdrop-bl)ur-xs z-25 top-[60px]">
         <div className=" w-[85%] sm:w-[90%] lg:w-[30%] ml-[14%] sm:ml-[9%] lg:ml-[14%] relative">
-          <div className="border-t rounded-xl flex flex-col bg-white sticky md:left-[65%] shadow-2xl md:top-[70px] z-40">
-            {usersSearch.length ? (
-              usersSearch.map((item, index) => {
-                if (index < 8) {
-                  return (
-                    <div
-                      onClick={() => handleClickUser(item)}
-                      className="flex items-center justify-between px-4 py-1 gap-4 hover:bg-gray-100 duration-200 cursor-pointer"
-                    >
-                      <div className="flex items-center gap-3">
-                        <MagnifyingGlassIcon
-                          strokeWidth="2"
-                          className="w-5 h-5"
-                        />
-                        <div className="flex items-center gap-1">
-                          <h5 className="font-semibold text-md">{item.name}</h5>
-                          <span>.</span>
-                          <h6 className="font-normal text-sm text-gray-800">
-                            {item.headLine}
-                          </h6>
+          <div className="border-t rounded-xl flex flex-col bg-white sticky md:left-[65%] shadow-2xl md:top-[70px] z-40 dark:bg-black dark:text-white">
+            {!isVide ? (
+              usersSearch.length ? (
+                usersSearch.map((item, index) => {
+                  if (index < 8) {
+                    return (
+                      <div
+                        onClick={() => handleClickUser(item)}
+                        className="flex items-center justify-between px-4 py-1 gap-4 hover:bg-gray-100 duration-200 cursor-pointer dark:hover:bg-gray-900"
+                      >
+                        <div className="flex items-center gap-3">
+                          <MagnifyingGlassIcon
+                            strokeWidth="2"
+                            className="w-5 h-5"
+                          />
+                          <div className="flex items-center gap-1">
+                            <h5 className="font-semibold text-md">
+                              {item.name}
+                            </h5>
+                            <span>.</span>
+                            <h6 className="font-normal text-sm text-gray-800 dark:text-white">
+                              {item.headLine}
+                            </h6>
+                          </div>
+                        </div>
+                        <div className="w-10 h-10 rounded-[50%]">
+                          <img
+                            src={`${serverURL}` + item.profile_picture}
+                            alt=""
+                            className="w-10 h-10 rounded-[50%]"
+                          />
                         </div>
                       </div>
-                      <div className="w-10 h-10 rounded-[50%]">
-                        <img
-                          src={`${serverURL}` + item.profile_picture}
-                          alt=""
-                          className="w-10 h-10 rounded-[50%]"
-                        />
-                      </div>
-                    </div>
-                  );
-                }
-              })
+                    );
+                  }
+                })
+              ) : (
+                <span className="bg-white dark:bg-black dark:text-white font-semibold text-md p-4">
+                  not founddd
+                </span>
+              )
             ) : (
               <div>
                 <div className="px-4 py-1 mb-1 w-full">
@@ -133,7 +144,7 @@ export const SearchModal = ({ query, openSearchModal }) => {
                     ))}
                   </div>
                 </div>
-                <div className="flex items-center gap-3 px-4 py-2 hover:bg-gray-100 duration-200 cursor-pointer ">
+                <div className="flex items-center gap-3 px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-900 duration-200 cursor-pointer ">
                   <ClockIcon strokeWidth="2" className="w-5 h-5" />
                   <h5 className="font-semibold text-md text-center">
                     kachoud said
@@ -142,15 +153,17 @@ export const SearchModal = ({ query, openSearchModal }) => {
               </div>
             )}
 
-            <div
-              onClick={() => {
-                openSearchModal(false);
-                navigate(`/search/results/all/${query}`);
-              }}
-              className="text-center text-[#0A66C2] font-semibold p-2 mt-2 border-t rounded-b-xl border-t-gray-100 hover:bg-gray-100 duration-200 cursor-pointer"
-            >
-              See all results
-            </div>
+            {!isVide && usersSearch.length && (
+              <div
+                onClick={() => {
+                  openSearchModal(false);
+                  navigate(`/search/results/all/${query}`);
+                }}
+                className="text-center dark:hover:bg-gray-950 text-[#0A66C2] font-semibold p-2 mt-2 border-t rounded-b-xl border-t-gray-100 hover:bg-gray-100 duration-200 cursor-pointer"
+              >
+                See all results
+              </div>
+            )}
           </div>
         </div>
       </div>
