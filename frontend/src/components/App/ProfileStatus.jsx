aa5911c28566d8882a3a3ec17efddd2e89861a64
sofyan <soufianeboukir0@gmail.com>
2025-03-29 18:58:21 +0000

    handle profile status

--- frontend/src/components/App/ProfileStatus.jsx
+++ frontend/src/components/App/ProfileStatus.jsx
@@ -1,6 +1,6 @@
-import React from 'react'
 import { Profile } from './Profile'
 import { AppSelector } from '../../selectors/AppSelector'
+import { SemiCircleProgress } from 'react-semicircle-progressbar'
 
 export const ProfileStatus = () => {
   const {userData} = AppSelector()
@@ -10,14 +10,19 @@ export const ProfileStatus = () => {
         <Profile />
         </div>
         <div className="bg-white dark:bg-black rounded-xl mt-2 flex justify-center gap-5 py-2">
-        <div className="text-center dark:hover:bg-gray-950 hover:bg-gray-50 duration-200 p-1 cursor-pointer">
-            <p className="font-semibold text-xl">{userData.followers?.length}</p>
-            <span>Followers</span>
+          <div className="text-center dark:hover:bg-gray-950 hover:bg-gray-50 duration-200 p-1 cursor-pointer">
+              <p className="font-semibold text-xl">{userData.followers?.length}</p>
+              <span>Followers</span>
+          </div>
+          <div className="text-center dark:hover:bg-gray-950 hover:bg-gray-50 duration-200 p-1 cursor-pointer">
+              <p className="font-semibold text-xl">{userData.following?.length}</p>
+              <span>Followings</span>
+          </div>
         </div>
-        <div className="text-center dark:hover:bg-gray-950 hover:bg-gray-50 duration-200 p-1 cursor-pointer">
-            <p className="font-semibold text-xl">{userData.following?.length}</p>
-            <span>Followings</span>
-        </div>
+
+
+        <div className="bg-white dark:bg-black rounded-xl mt-2 px-3 py-2">
+          <h1 className='text-2xl font-semibold'>Profile status</h1>
         </div>
     </div>
   )
